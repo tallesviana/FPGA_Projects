2f80c73414ed15f85bd6ebfac349bc7406fef280
tallesviana <tallesviana1@gmail.com>
2018-06-06 00:02:14 -0300

    WEEK 14: PROJECT: Extras: Finish tuner top level file and visual effects
    - !!!!!  Untested  !!!!!

--- PROJECT/Audiobocs/source/tuner.vhd
+++ PROJECT/Audiobocs/source/tuner.vhd
@@ -1,16 +1,31 @@
 LIBRARY ieee;
-  use ieee.std_logic_1164.all;
+    use ieee.std_logic_1164.all;
+    use ieee.numeric_std.all;
+
+------------------------------------------------
+--     ENTITY       ----------------------------
+------------------------------------------------
 
 ENTITY tuner IS
-  PORT(
-      RESET_N, CLK :  std_logic;
+    PORT(
+        RESET_N, CLK :  IN std_logic;
 
-      AUDIO_I      :  std_logic_vector(15 downto 0);
+        tuner_AUDIO_I      :  IN std_logic_vector(15 downto 0);
 
-      NOTE_O       :  std_logic_vector(2 downto 0);
-      DELTA_O      :  std_logic_vector(16 downto 0)
+        tuner_HEX0_O :  OUT  STD_LOGIC_VECTOR(6 DOWNTO 0);  -- HEX0_N on the top level
+        tuner_HEX1_O :  OUT  STD_LOGIC_VECTOR(6 DOWNTO 0);
+        tuner_HEX2_O :  OUT  STD_LOGIC_VECTOR(6 DOWNTO 0);
+        tuner_HEX3_O :  OUT  STD_LOGIC_VECTOR(6 DOWNTO 0);
+
+        tuner_LEDR_O :  OUT  STD_LOGIC_VECTOR(9 DOWNTO 0);  --LEDR_0 on the top level
+        tuner_LEDG_O :  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0)
+
   );
 END tuner;
+
+------------------------------------------------
+--     ARCHITECTURE ----------------------------
+------------------------------------------------
 
 ARCHITECTURE struct OF tuner IS
 
@@ -31,13 +46,42 @@ ARCHITECTURE struct OF tuner IS
   );
   END COMPONENT;
 
+  COMPONENT tuner_visual IS
+  PORT(
+    TONE_I  :  IN  std_logic_vector(2 downto 0);
+    DELTA_I :  IN  std_logic_vector(16 downto 0);
+
+    HEX0_O :  OUT  STD_LOGIC_VECTOR(6 DOWNTO 0);  -- HEX0_N on the top level
+    HEX1_O :  OUT  STD_LOGIC_VECTOR(6 DOWNTO 0);
+    HEX2_O :  OUT  STD_LOGIC_VECTOR(6 DOWNTO 0);
+    HEX3_O :  OUT  STD_LOGIC_VECTOR(6 DOWNTO 0);
+
+    LEDR_O :  OUT  STD_LOGIC_VECTOR(9 DOWNTO 0);  --LEDR_0 on the top level
+    LEDG_O :  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0)
+  );
+END COMPONENT;
+
+------------------------------------------------
+--     SIGNALS     ----------------------------
+------------------------------------------------
+
   SIGNAL t_square_wave  :  std_logic;
+  SIGNAL t_note         :  std_logic;
+  SIGNAL t_delta        :  std_logic;
+
+  SIGNAL t_hex          :  std_logic_vector(27 downto 0);
+  SIGNAL t_ledr         :  std_logic_vector(9 downto 0);
+  SIGNAL t_ledg         :  std_logic_vector(7 downto 0);
+
+------------------------------------------------
+--     BEGINNIG     ----------------------------
+------------------------------------------------
 
 BEGIN
 
 sq_gen: square_gen
   PORT MAP(
-      audio_i        => AUDIO_I,
+      audio_i        => tuner_AUDIO_I,
       square_wave_o  =>  t_square_wave
   );
 
@@ -46,8 +90,30 @@ tuner_count: tuner_tone_by_count
       clk       => CLK,
       reset_n   => RESET_N,
       wave_i    => t_square_wave,
-      note_o    => NOTE_O,
-      delta_o   => DELTA_O
+      note_o    => t_note,
+      delta_o   => t_delta
   );
+
+visual: tuner_visual
+    PORT MAP(
+        TONE_I  => t_note,
+        DELTA_I => t_delta,
+
+        HEX0_O  => t_hex(6 downto 0),
+        HEX1_O  => t_hex(13 downto 7),
+        HEX2_O  => t_hex(20 downto 14),
+        HEX3_O  => t_hex(27 downto 21),
+
+        LEDR_O  =>  t_ledr,
+        LEDG_O  =>  t_ledg
+    );
+
+tuner_HEX0_O  <= t_hex(6 downto 0);
+tuner_HEX1_O  <= t_hex(13 downto 7);
+tuner_HEX2_O  <= t_hex(20 downto 14);
+tuner_HEX3_O  <= t_hex(27 downto 21);
+
+tuner_LEDR_O  <=  t_ledr;
+tuner_LEDG_O  <=  t_ledg;
 
 END struct;
--- PROJECT/Audiobocs/source/tuner_visual.vhd
+++ PROJECT/Audiobocs/source/tuner_visual.vhd
@@ -3,3 +3,90 @@ LIBRARY ieee;
   use ieee.numeric_std.all;
 
 ENTITY tuner_visual IS
+  PORT(
+    TONE_I  :  IN  std_logic_vector(2 downto 0);
+    DELTA_I :  IN  std_logic_vector(16 downto 0);
+
+    HEX0_O :  OUT  STD_LOGIC_VECTOR(6 DOWNTO 0);  -- HEX0_N on the top level
+    HEX1_O :  OUT  STD_LOGIC_VECTOR(6 DOWNTO 0);
+    HEX2_O :  OUT  STD_LOGIC_VECTOR(6 DOWNTO 0);
+    HEX3_O :  OUT  STD_LOGIC_VECTOR(6 DOWNTO 0);
+
+    LEDR_O :  OUT  STD_LOGIC_VECTOR(9 DOWNTO 0);  --LEDR_0 on the top level
+    LEDG_O :  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0)
+  );
+END tuner_visual;
+
+ARCHITECTURE rtl OF tuner_visual IS
+
+-- Signals & Constants Declaration
+  CONSTANT note_bar   : std_logic_vector(27 downto 0):= "1000000_1000000_1000000_1000000";
+	CONSTANT note_E2 		: std_logic_vector(27 downto 0):= "0000000_1111001_1011011_0000000";
+  CONSTANT note_A2 		: std_logic_vector(27 downto 0):= "0000000_1110111_1011011_0000000";
+	CONSTANT note_D3 		: std_logic_vector(27 downto 0):= "0000000_1011110_1001111_0000000";
+  CONSTANT note_G3 		: std_logic_vector(27 downto 0):= "0000000_0111101_1001111_0000000";
+  CONSTANT note_B3 		: std_logic_vector(27 downto 0):= "0000000_1111100_1001111_0000000";
+  CONSTANT note_E4 		: std_logic_vector(27 downto 0):= "0000000_1111001_1100110_0000000"; 
+
+  SIGNAL ledg_value      :   std_logic_vector(7 downto 0);
+  SIGNAL ledr_diff_value :   unsigned(9 downto 0);
+  SIGNAL tuned_value:   unsigned(9 downto 0) := "0000110000";  -- Gonna shift this value, depends on delta!
+  SIGNAL hex_seg    :   std_logic_vector(27 downto 0);
+
+BEGIN
+
+  -- GETS NOTE - AND DISPLAYS ON 7SEG
+  tone: PROCESS(ALL)
+    BEGIN 
+      CASE TONE_I IS
+        WHEN "001" =>   hex_seg <= note_E2;
+        WHEN "010" =>   hex_seg <= note_A2;
+        WHEN "011" =>   hex_seg <= note_D3;
+        WHEN "100" =>   hex_seg <= note_G3;
+        WHEN "101" =>   hex_seg <= note_B3;
+        WHEN "110" =>   hex_seg <= note_E4;
+        WHEN OTHERS =>  hex_seg <= note_bar;
+      END CASE;
+    END PROCESS;
+
+  --  GETS THE DELTA - AND SHOW IN THE LEDS
+  delta: PROCESS(ALL)
+    variable diff : natural;
+    BEGIN
+
+      IF abs(signed(DELTA_I)) > 6340 THEN
+        diff := 5;
+      ELSIF abs(signed(DELTA_I)) > 3125 THEN
+        diff := 4;
+      ELSIF abs(signed(DELTA_I)) > 1250 THEN
+        diff := 3;
+      ELSIF abs(signed(DELTA_I)) > 500 THEN
+        diff := 2;
+      ELSIF abs(signed(DELTA_I)) > 200 THEN
+        diff := 1;
+      ELSE
+        diff := 0;
+        ledg_value <= (OTHERS => '1');  -- TUNED !!!
+      END IF;
+
+      CASE DELTA_I(DELTA_I'left) IS    -- Checking if positive or negative
+        WHEN '1' => -- Negative -- Means that my count is greater than the real note
+          ledr_diff_value <= shift_left(tuned_value, diff);
+        WHEN '0' =>
+          ledr_diff_value <= shift_right(tuned_value, diff);
+        WHEN OTHERS =>
+          ledr_diff_value <= (OTHERS => '0');
+      END CASE;
+
+    END PROCESS;
+
+    -- CONCURRENT ASSIGNMENTS --
+    HEX0_O <= not(hex_seg(6 downto 0));
+    HEX1_O <= not(hex_seg(13 downto 7));
+    HEX2_O <= not(hex_seg(20 downto 14));
+    HEX3_O <= not(hex_seg(27 downto 21));
+
+    LEDR_O <= ledr_diff_value;
+    LEDG_O <= ledg_value;
+
+END rtl;
